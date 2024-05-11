8d9cdda02845a9402a22808e2ee28a19313520af
Umut Berke Pezük <umutberkepezuk@gmail.com>
2024-05-12 02:06:35 +0300

    image added

--- CoinList/Cell/CollectionViewCell.swift
+++ CoinList/Cell/CollectionViewCell.swift
@@ -6,11 +6,13 @@
 //
 
 import UIKit
+import Kingfisher
 
 class CollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var percentageLabel: UILabel!
-    @IBOutlet weak var priceBtcLabel: UILabel!
+    @IBOutlet weak var btcPriceLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var symbol: UILabel!
@@ -18,14 +20,27 @@ class CollectionViewCell: UICollectionViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
     
-    func configure() {
-        nameLabel.text = "Bitcoin"
-        symbol.text = "BTC"
-        priceLabel.text = "1000"
-        priceBtcLabel.text = "0.0001"
-        percentageLabel.text = "%37m"
+    func configure(with coin: Coin) {
+        nameLabel.text = coin.name
+        symbol.text = coin.symbol
+        priceLabel.text = coin.price
+        btcPriceLabel.text = coin.btcPrice
+        percentageLabel.text = coin.change
+        let modifiedURL = coin.iconURL.replacingLast3Characters(with: "png")
+        let url = URL(string: modifiedURL)
+        image.kf.setImage(with: url)
+    }
+
+}
+extension String {
+    func replacingLast3Characters(with newEnding: String) -> String {
+        guard self.count >= 3 else { return self }
+        let index = self.index(self.endIndex, offsetBy: -3)
+        let start = self[..<index]
+        return start + newEnding
     }
 }
+
